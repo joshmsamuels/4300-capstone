0e061b8829984eb8bbe8d061bceac7c2ebdba27f
Josh <joshsamuels04@gmail.com>
2020-12-06 13:27:07 -0500

    Created SignIn Component (#1)
    * Renamed verifyEmail component to guestSignIn to be more representative of component and Added alerts that sso via providers is unavailable
    
    * finished guest sign in path and added sign out button

---
+++ src/components/SignIn/GuestSignIn/index.js
@@ -0,0 +1,1 @@
+export { GuestSignIn } from './GuestSignIn'
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { Modal } from './Modal'
 export { NotificationRegistration } from './NotificationRegistration'
 export { Search } from './Search'
 export { SignIn } from './SignIn'
