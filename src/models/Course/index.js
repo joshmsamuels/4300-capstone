1581c9211ade5cf8abcbedf8a06260ad64ef0872
Josh <joshsamuels04@gmail.com>
2020-12-09 21:55:09 -0500

    Add eslint (#3)
    * Added eslint config
    
    * fixed eslint errors
    
    * added linting workflow

--- src/models/Course/index.js
+++ src/models/Course/index.js
@@ -1,29 +1,29 @@
 export default class Course {
     constructor({
-      available,
-      capacity,
-      code, 
-      credits,
-      level,
-      location,
-      meetingInfo,
-      name, 
-      professor,
-      section,
-      status,
-      term,
+        available,
+        capacity,
+        code, 
+        credits,
+        level,
+        location,
+        meetingInfo,
+        name, 
+        professor,
+        section,
+        status,
+        term,
     }) {
-      this.available = available
-      this.capacity = capacity
-      this.code = code 
-      this.credits = credits
-      this.level = level
-      this.location = location
-      this.meetingInfo = meetingInfo
-      this.name = name
-      this.professor = professor
-      this.section = section
-      this.status = status
-      this.term = term
+        this.available = available
+        this.capacity = capacity
+        this.code = code 
+        this.credits = credits
+        this.level = level
+        this.location = location
+        this.meetingInfo = meetingInfo
+        this.name = name
+        this.professor = professor
+        this.section = section
+        this.status = status
+        this.term = term
     }
-  }  
+}  
